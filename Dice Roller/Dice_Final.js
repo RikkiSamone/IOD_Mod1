97e58ca21c343632eaa8e26c6cca6e1a3a6ec0da
Rikki <rikkisamone@gmail.com>
2024-10-15 15:32:41 -0700

    separating dice roller project

--- Dice Roller/Dice_Final.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("rollButton").addEventListener("click", function () {
-  const diceType = parseInt(document.getElementById("dice-select").value);
-  const diceValue = Math.floor(Math.random() * diceType) + 1; // Generate a number based on selected dice type
-  document.getElementById("dice").textContent = diceValue; // Display the result
-  document.getElementById(
-    "result"
-  ).textContent = `You rolled a ${diceValue} on a d${diceType}!`; // Show the result message
-});
